ba68916cd09f48e2524f348860ee31a9a969ab34
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-21 02:59:34 +0700

    Widget build, Padding

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page', style: TextStyle(fontSize: 24)),
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [],
+            ),
+          ),
+        ),
       ),
     );
   }
