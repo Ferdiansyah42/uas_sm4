957cd62fa547ce4cb7ac078ece71531c3599feb0
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-20 23:33:34 +0700

    scan page

---
+++ lib/pages/scan_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Scan Page', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
