d24d287bf27d2e008e55d4e9e8a30a4e4fe82d73
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-23 00:10:05 +0700

    menu profil

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -8,15 +8,92 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  bool isDarkModeEnabled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFEAF2FF),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: const [
-          // Nanti isi di sini
-        ],
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            const SizedBox(height: 20),
+
+            Center(
+              child: CircleAvatar(
+                radius: 45,
+                backgroundColor: Colors.transparent,
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/profil.png',
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Nama
+            const Center(
+              child: Text(
+                'Ferdi',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            const Divider(thickness: 1),
+            const SizedBox(height: 10),
+
+            ListTile(
+              leading: const Icon(Icons.person, color: Colors.black),
+              title: const Text(
+                'Pengaturan Profil',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.history, color: Colors.black),
+              title: const Text(
+                'History Transaction',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {},
+            ),
+
+            SwitchListTile(
+              secondary: const Icon(Icons.dark_mode, color: Colors.black),
+              title: const Text(
+                'Mode Gelap',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              value: isDarkModeEnabled,
+              onChanged: (val) {
+                setState(() {
+                  isDarkModeEnabled = val;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
