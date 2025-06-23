74c6bb1bb5a7b3265694636d307c8a2d7c205259
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-24 03:08:04 +0700

    membuat container dan isinya

--- lib/pages/terima_page.dart
+++ lib/pages/terima_page.dart
@@ -7,22 +7,59 @@ class TerimaPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFEAF2FF),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'menerima uang',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+      appBar: AppBar(
+        title: const Text('Terima'),
+        centerTitle: true, 
+        backgroundColor: const Color.fromARGB(255, 109, 68, 180),
+        automaticallyImplyLeading: false,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 5,
+                  offset: const Offset(0, 2),
+                )
+              ],
             ),
-            SizedBox(height: 10),
-            Text(
-              'Tunjukkan QR Code Anda kepada pengirim',
-              style: TextStyle(fontSize: 14, color: Colors.black54),
-              textAlign: TextAlign.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Terima Cepat',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 12),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Cari nama teman ',
+                    prefixIcon: const Icon(Icons.search),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const Text(
+                  'Buat permintaan pertama',
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
