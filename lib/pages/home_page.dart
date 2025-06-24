0360947bce95f2d633d2c5aa5fadf4b88c6ade68
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-24 12:07:07 +0700

    nyambungin voucher game ke home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ import 'toko_online_page.dart';
 import 'film_movies_page.dart';
 import 'pesan_makanan_page.dart';
 import 'toko_elektronik_page.dart';
+import 'voucher_game_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -193,7 +194,9 @@ class _HomePageState extends State<HomePage> {
                                 Navigator.push(context, MaterialPageRoute(builder: (_) => const PesanMakananPage()));
                               } else if (index == 3) {
                                 Navigator.push(context, MaterialPageRoute(builder: (_) => const TokoElektronikPage()));
-                                }
+                              } else if (index == 4) {
+                                Navigator.push(context, MaterialPageRoute(builder: (_) => const VoucherGamePage()));
+                              }
                             },
                             child: _buildServiceItem(
                               icons[index],
