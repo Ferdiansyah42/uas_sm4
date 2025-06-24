52167d53c35ade5a9aac77fe9592f2427df2a6ac
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-24 15:27:40 +0700

    menyertakan Api

--- lib/pages/pesan_makanan_page.dart
+++ lib/pages/pesan_makanan_page.dart
@@ -1,26 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class PesanMakananPage extends StatelessWidget {
+class PesanMakananPage extends StatefulWidget {
   const PesanMakananPage({super.key});
+
+  @override
+  State<PesanMakananPage> createState() => _PesanMakananPageState();
+}
+
+class _PesanMakananPageState extends State<PesanMakananPage> {
+  List<dynamic> makananList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMakanan();
+  }
+
+  Future<void> fetchMakanan() async {
+    final url = Uri.parse('https://www.themealdb.com/api/json/v1/1/search.php?f=c'); // ambil makanan huruf 'c'
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        makananList = data['meals'] ?? [];
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+      // Error handling bisa ditambahkan di sini
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFEAF2FF),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text(
-          'Pesan Makanan',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('Pesan Makanan', style: TextStyle(color: Colors.white)),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 126, 80, 206),
+        backgroundColor: Colors.deepPurple,
+        automaticallyImplyLeading: false,
       ),
-      body: const Center(
-        child: Text(
-          'Ini halaman Pesan Makanan',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      
     );
   }
 }
