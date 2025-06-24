640717b98794d699d2d9c16de7bb933eead27a05
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-06-24 11:30:04 +0700

    pesan makanan

---
+++ lib/pages/pesan_makanan_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PesanMakananPage extends StatelessWidget {
+  const PesanMakananPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Menghilangkan panah kiri
+        title: const Text(
+          'Pesan Makanan',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 126, 80, 206),
+      ),
+      body: const Center(
+        child: Text(
+          'Ini halaman Pesan Makanan',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
